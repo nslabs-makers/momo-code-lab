772b1a17008d41db3a23e4a7231772a773bd4cee
nslabs-makers <82530587+nslabs-makers@users.noreply.github.com>
2021-07-22 15:40:45 -0700

    Add files via upload

---
+++ momo_blocks.js
@@ -0,0 +1,13 @@
+Blockly.Blocks['move_momo'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("Move")
+        .appendField(new Blockly.FieldDropdown([["Forward","http://momo.local:8080/motors/forward"], ["Back","http://momo.local:8080/motors/back"], ["Left","http://momo.local:8080/motors/left"], ["Right","http://momo.local:8080/motors/right"]]), "Direction");
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(230);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
