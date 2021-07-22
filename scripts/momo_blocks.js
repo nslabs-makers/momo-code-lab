be78ea52c7af669331450e7be420f06434ebd37a
nslabs-makers <82530587+nslabs-makers@users.noreply.github.com>
2021-07-22 15:31:18 -0700

    Update momo_blocks.js

--- scripts/momo_blocks.js
+++ scripts/momo_blocks.js
@@ -1,31 +1,31 @@
 Blockly.defineBlocksWithJsonArray({
   "type": "move_momo",
-  "message0": "%1 %2",
+  "message0": "Move %1",
   "args0": [
     {
       "type": "field_dropdown",
-      "name": "direction",
+      "name": "Direction",
       "options": [
         [
           "Forward",
-          "forward"
+          "http://momo.local:8080/motors/forward"
         ],
         [
           "Back",
-          "back"
+          "http://momo.local:8080/motors/back"
         ],
         [
           "Left",
-          "left"
+          "http://momo.local:8080/motors/left"
         ],
         [
           "Right",
-          "right"
+          "http://momo.local:8080/motors/right"
         ]
       ]
-    },
-  
+    }
   ],
+  "inputsInline": true,
   "previousStatement": null,
   "nextStatement": null,
   "colour": 60,
