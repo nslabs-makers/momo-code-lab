10532c1e0575794109602c143a58e35988405160
nslabs-makers <82530587+nslabs-makers@users.noreply.github.com>
2021-07-22 15:36:05 -0700

    Update momo_blocks.js

--- scripts/momo_blocks.js
+++ scripts/momo_blocks.js
@@ -1,34 +1,13 @@
-Blockly.defineBlocksWithJsonArray([{
-  "type": "move_momo",
-  "message0": "Move %1",
-  "args0": [
-    {
-      "type": "field_dropdown",
-      "name": "Direction",
-      "options": [
-        [
-          "Forward",
-          "http://momo.local:8080/motors/forward"
-        ],
-        [
-          "Back",
-          "http://momo.local:8080/motors/back"
-        ],
-        [
-          "Left",
-          "http://momo.local:8080/motors/left"
-        ],
-        [
-          "Right",
-          "http://momo.local:8080/motors/right"
-        ]
-      ]
-    }
-  ],
-  "inputsInline": true,
-  "previousStatement": null,
-  "nextStatement": null,
-  "colour": 60,
-  "tooltip": "",
-  "helpUrl": ""
-}]);
+Blockly.Blocks['move_momo'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("Move")
+        .appendField(new Blockly.FieldDropdown([["Forward","http://momo.local:8080/motors/forward"], ["Back","http://momo.local:8080/motors/back"], ["Left","http://momo.local:8080/motors/left"], ["Right","http://momo.local:8080/motors/right"]]), "Direction");
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(230);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
